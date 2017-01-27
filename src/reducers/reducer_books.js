d16495d3b31eb693631cadd3dbfcfcaf48244a23
Zarathon Maia <zarathonmaia@gmail.com>
2017-01-27 15:18:37 -0300

    Initital commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Abundance'},
+    {title: 'Tdd as well'},
+    {title: 'How to make a cake'},
+    {title: 'Assassins Creed: The book art'}
+  ];
+}
